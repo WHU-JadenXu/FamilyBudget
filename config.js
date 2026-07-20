07509e71e5efd145e52d5c6a17b9babbc15a23b9
Xu Jiadan <2023202140044@whu.edu.cn>
2026-07-20 11:20:44 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.LEDGER_CONFIG = {
   SUPABASE_URL: "https://ehsuayzplakzhkkspoel.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoc3VheXpwbGFremhra3Nwb2VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1MDI4OTEsImV4cCI6MjEwMDA3ODg5MX0.hwhY0g5Vog-ERD9XxJmkcSP5cv8bmNDaUMNGLMFnzS0",
   FAMILY_ID: "li-xu-family",
-  SITE_URL: ""
+  SITE_URL: "https://whu-jadenxu.github.io/FamilyBudget/"
 };
